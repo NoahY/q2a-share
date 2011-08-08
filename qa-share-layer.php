3da868536af79bda0c326afc8440c6464f1c62c2
NoahY <yuttadhammo@yahoo.com>
2011-08-08 15:43:25 +0530

    manual clean email

--- qa-share-layer.php
+++ qa-share-layer.php
@@ -63,8 +63,12 @@
 				$text = qa_opt('share_plugin_suggest_text');
 
 				$text = str_replace('#',$buttons,$text);
-
-				$text = preg_replace('/@([^@]+)@/','<a href="mailto:?'.rawurlencode('subject='.'['.qa_opt('site_title').'] '.$q_view['raw']['title']).'&body='.rawurlencode(qa_path_html(qa_q_request($q_view['raw']['postid'], $q_view['raw']['title']), null, qa_opt('site_url'))).'">$1</a>',$text);
+				
+				$subject = str_replace('&','%26','['.qa_opt('site_title').'] '.$q_view['raw']['title']);
+				
+				$body = str_replace('&','%26',qa_path_html(qa_q_request($q_view['raw']['postid'], $q_view['raw']['title']), null, qa_opt('site_url')));
+				
+				$text = preg_replace('/@([^@]+)@/','<a href="mailto:?subject='.$subject.'&body='.$body.'">$1</a>',$text);
 				
 				$this->output_raw($text);
 				
