b50e6d1017812a4f996dcc2c3f928ea5478d1f9d
NoahY <yuttadhammo@yahoo.com>
2011-08-08 15:24:07 +0530

    sanitize subject of email

--- qa-share-layer.php
+++ qa-share-layer.php
@@ -64,7 +64,7 @@
 
 				$text = str_replace('#',$buttons,$text);
 
-				$text = preg_replace('/@([^@]+)@/','<a href="mailto:?subject='.'['.qa_opt('site_title').'] '.$q_view['raw']['title'].'&body='.qa_path_html(qa_q_request($q_view['raw']['postid'], $q_view['raw']['title']), null, qa_opt('site_url')).'">$1</a>',$text);
+				$text = preg_replace('/@([^@]+)@/','<a href="mailto:?subject='.qa_html('['.qa_opt('site_title').'] '.$q_view['raw']['title']).'&body='.qa_path_html(qa_q_request($q_view['raw']['postid'], $q_view['raw']['title']), null, qa_opt('site_url')).'">$1</a>',$text);
 				
 				$this->output_raw($text);
 				
