d6ca09352b8dd1e8825a2596fdc6f802f7606c07
NoahY <yuttadhammo@yahoo.com>
2011-08-08 10:09:09 +0530

    hard code buttons

--- qa-share-layer.php
+++ qa-share-layer.php
@@ -16,18 +16,31 @@
 			}
 			
 		}
-		
+
+		function q_view_buttons($q_view)
+		{
+			if (!empty($q_view['form'])) {
+				$this->form($q_view['form']);
+			}
+		}		
 		function q_view_buttons($q_view) {
+			$this->output('<DIV CLASS="qa-q-view-buttons">');
+			if (!empty($q_view['form'])) {
+				$this->form($q_view['form']);
+			}
+			
 			$url = qa_path_html(qa_q_request($this->content['raw']['postid'], $this->content['title']), null, qa_opt('site_url'));
 			
+			$output = '<div id="qa-share-buttons">';
+			
 			if((bool)qa_opt('share_plugin_facebook')) {
-				$q_view['form']['custom-facebook'] = '<iframe src="http://www.facebook.com/plugins/like.php?app_id=143472095738441&amp;href='.$url.'&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe>';
+				$output .= '<iframe src="http://www.facebook.com/plugins/like.php?app_id=143472095738441&amp;href='.$url.'&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe>';
 			}
 			if((bool)qa_opt('share_plugin_twitter')) {
-				$q_view['form']['custom-twitter'] = '<a href="http://twitter.com/share" class="twitter-share-button" data-count="none">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>';
+				$output .= '<a href="http://twitter.com/share" class="twitter-share-button" data-count="none">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>';
 			}
 			if((bool)qa_opt('share_plugin_google')) {
-				$q_view['form']['custom-google'] = '<g:plusone count="false"></g:plusone>
+				$output .= '<g:plusone count="false"></g:plusone>
 <script type="text/javascript">
   (function() {
     var po = document.createElement(\'script\'); po.type = \'text/javascript\'; po.async = true;
@@ -37,9 +50,12 @@
 </script>';
 			}
 			if((bool)qa_opt('share_plugin_linkedin')) {
-				$q_view['form']['custom-linkedin'] = '<script type="text/javascript" src="http://platform.linkedin.com/in.js"></script><script type="in/share"></script>';
+				$output .= '<script type="text/javascript" src="http://platform.linkedin.com/in.js"></script><script type="in/share"></script>';
 			}
-			qa_html_theme_base::q_view_buttons($q_view);
+			
+			$this->output_raw($output);
+			
+			$this->output('</div></DIV>');
 		}
 	}
 	
