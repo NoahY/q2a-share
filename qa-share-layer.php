8458a2d22a03f931a4194dff3585299121c1393f
NoahY <noahy57@gmail.com>
2011-10-14 00:05:03 +0530

    toggle footer scripts

--- qa-share-layer.php
+++ qa-share-layer.php
@@ -81,18 +81,22 @@
 		
 		function footer() {
 			qa_html_theme_base::footer();
-			$this->output('<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {return;}
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, "script", "facebook-jssdk"));
-
-</script><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
-
-<script type="text/javascript">(function() {var po = document.createElement("script"); po.type = "text/javascript"; po.async = true;po.src = "https://apis.google.com/js/plusone.js";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(po, s);})();</script>');
+			if(qa_opt('share_plugin_facebook')) {
+				$this->output('<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) {return;}
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, "script", "facebook-jssdk"));</script>');
+			}
+			if(qa_opt('share_plugin_twitter')) {
+				$this->output('<script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>');
+			}
+			if(qa_opt('share_plugin_google')) {
+				$this->output('<script type="text/javascript">(function() {var po = document.createElement("script"); po.type = "text/javascript"; po.async = true;po.src = "https://apis.google.com/js/plusone.js";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(po, s);})();</script>');
+			}
 			
 		}
 		
